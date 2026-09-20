7bda82cc4a457b60849c72f25cf054f864dfb2b0
JAIVIK chauhan <129761207+JAIVIK-chauhan@users.noreply.github.com>
2026-09-20 21:54:15 +0530

    Time: 0 ms (100%), Space: 36.8 MB (11.74%) - LeetHub

---
+++ 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countIntersectingIntervals(vector<vector<int>>& intervals) {
+         long long p = 0;
+
+        sort(intervals.begin(), intervals.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        pq.push(intervals[0][1]);
+        int i = 1;
+
+        while(i < intervals.size()){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            while(!pq.empty() && pq.top() < start) pq.pop();
+            p = p + pq.size();
+
+            pq.push(end);
+            i++;
+        }
+        return p;
+    }
+};
